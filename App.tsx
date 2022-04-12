5ebd957f58ccddf252d7c26becbf2e817449e000
dober <huaycoin@betkub.com>
2022-04-12 14:25:37 +0700

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [enteredCharacter, setEnteredCharacter] = useState("");
+  const [characters, setCharacters] = useState<string[]>([]);
+
+  function characterInputHandler(enteredText: string) {
+    setEnteredCharacter(enteredText);
+  }
+
+  function addCharacterHandler() {
+    setCharacters((prevCharacters) => [...prevCharacters, enteredCharacter]);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Your Character Name"
+          style={styles.textInput}
+          onChangeText={characterInputHandler}
+        />
+        <Button title="Submit" onPress={addCharacterHandler} />
+      </View>
+      <View style={styles.characterContainer}>
+        {characters.map((character) => (
+          <View key={character} style={styles.characterItem}>
+            <Text>{character}</Text>
+          </View>
+        ))}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderColor: "#ccc",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "80%",
+    marginRight: 8,
+    padding: 8,
+  },
+  characterContainer: {
+    flex: 5,
+  },
+  characterItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+    color: "white",
   },
 });
