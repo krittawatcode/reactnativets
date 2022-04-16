b32761c4426d8099a740cf66be18df4fc3135b60
dober <huaycoin@betkub.com>
2022-04-16 16:39:43 +0700

    ass style for delete

--- components/GoalItem.tsx
+++ components/GoalItem.tsx
@@ -1,21 +1,20 @@
 import { StyleSheet, View, Text, Pressable } from "react-native";
 
 function GoalItem(props: any) {
-  // function addGoalHandler() {
-  //   props.onAddGoal(enteredGoalText);
-  //   setEnteredGoalText("");
-  // }
-
   function onDeleteItem() {
     props.onDeleteItem(props.item.id);
   }
 
   return (
-    <Pressable onPress={onDeleteItem}>
-      <View style={styles.goalItem}>
+    <View style={styles.goalItem}>
+      <Pressable
+        android_ripple={{ color: "#dddddd" }}
+        onPress={onDeleteItem}
+        style={({ pressed }) => pressed && styles.pressedItem}
+      >
         <Text style={styles.goalText}>{props.item.text}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 }
 
@@ -24,11 +23,16 @@ export default GoalItem;
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
+
     borderRadius: 6,
     backgroundColor: "#5e0acc",
   },
   goalText: {
     color: "white",
+    padding: 8,
+  },
+
+  pressedItem: {
+    opacity: 0.5,
   },
 });
